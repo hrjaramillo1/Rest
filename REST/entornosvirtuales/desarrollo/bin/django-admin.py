457062e62d8daa43c3e81e63f874dd7820930afb
accardenas4 <any.cardenas17@gmail.com>
2017-01-23 11:37:18 -0500

    Trabajo en clase ingreso estudiantes

---
+++ REST/entornosvirtuales/desarrollo/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/rafael/entornosvirtuales/desarrollo/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ REST/entornosvirtuales/desarrollo/proyectos-ac/institucion/escuela/migrations/0002_auto_20170123_1054.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('escuela', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Estudiante',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('nombre', models.CharField(default=b'', max_length=100, blank=True)),
+                ('apellido', models.CharField(default=b'', max_length=100, blank=True)),
+                ('cedula', models.CharField(default=b'', max_length=100, blank=True)),
+                ('ciudad', models.CharField(default=b'', max_length=100, blank=True)),
+                ('provincia', models.CharField(default=b'', max_length=100, blank=True)),
+            ],
+            options={
+                'ordering': ('created',),
+            },
+        ),
+        migrations.DeleteModel(
+            name='Materia',
+        ),
+    ]
